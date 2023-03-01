2cf41b27261761ae63878cde6728825a85026073
nico vega <nv89@hotmail.com>
2023-03-01 14:05:24 -0300

    add discount controller + docker support

--- src/Services/Discount/Discount.API/Controllers/DiscountController.cs
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -13,7 +13,7 @@ namespace Discount.API.Controllers
     [Route("api/v1/[controller]")]
     public class DiscountController : ControllerBase
     {
-        private readonly IDiscountRepository _repository
+        private readonly IDiscountRepository _repository;
         public DiscountController(IDiscountRepository repository)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
